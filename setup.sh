eaa66e7cb27dbeef75db38f2a9f6cfc02f5bb9cd
Toshi Hayashi <hayashi@ntti3.com>
2016-04-07 00:55:47 +0000

    Fix Readme

--- setup.sh
+++ setup.sh
@@ -9,18 +9,12 @@ set -xe
 
 if [ "$(uname)" == "Darwin" ]; then
     brew install npm
-    sudo gem install compass
 else
     apt-get update
-    apt-get -y install ruby-compass
 fi
 
 if ! [ -e app/config.json ]; then
     cp app/config.json.sample app/config.json
 fi
-
-npm install -g karma
-
-npm install webpack webpack-dev-server
 
 npm install
