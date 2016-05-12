8e0662386871efdd166ca342d901b8fba70f96d5
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-05-12 11:32:53 +0200

    Fix default properties for array of objects.

--- app/js/models/schemaModel.js
+++ app/js/models/schemaModel.js
@@ -479,11 +479,17 @@ export default class SchemaModel extends Model {
           schema[key].subSchema = this.toFormJSON(value.items);
         }
       } else if (value.type === 'object') {
-        schema[key].type = 'Object';
-        schema[key].subSchema = this.toFormJSON(value);
+        if (value.properties) {
+          schema[key].type = 'Object';
+          schema[key].subSchema = this.toFormJSON(value);
+        } else {
+          schema[key].type = 'CodeEditor';
+          schema[key].format = value.format;
+        }
       } else if (value.type === 'boolean') {
         schema[key].type = 'Checkbox';
       }
+
       if (json.required !== undefined &&
         json.required.includes(key)) {
         schema[key].validators.push('required');
@@ -591,7 +597,7 @@ export default class SchemaModel extends Model {
           reject(error);
         });
       } else {
-        result.type = 'string';
+        result.type = schema.type;
         result.format = 'yaml';
         result.originalType = 'object';
         resolve(result);
@@ -606,7 +612,7 @@ export default class SchemaModel extends Model {
    */
   defaultValue(schema) {
     if (schema.type === 'object') {
-      if (schema.default === undefined) {
+      if (schema.default === undefined && schema.properties !== undefined) {
         const result = {};
 
         for (let key in schema.properties) {
@@ -623,6 +629,7 @@ export default class SchemaModel extends Model {
         }
         return [result];
       }
+      return undefined;
     }
     return schema.default;
   }
