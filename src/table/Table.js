db7c7b1f1f9918d0e3af7384bbe93943210b7bda
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-09-01 14:40:54 +0200

    Added table component with reducer and fetching data from API.

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,29 +1,86 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
 
+import {fetchData, clearData} from './TableActions';
 
 class Table extends Component {
+
+  componentWillMount() {
+    this.props.fetchData(this.props.schema.url, this.props.schema.plural);
+  }
+
+  componentWillUnmount() {
+    this.props.clearData();
+  }
+
   render() {
-    console.log(this.props);
+    const {schema} = this.props.schema;
+
     return (
-      <div>
-        Table component.
-      </div>
+      <table>
+        <thead>
+          <tr>
+            {schema.propertiesOrder.map((item, index) => {
+              if (item === 'id') {
+                return null;
+              }
+              return (
+                <th key={index}>{schema.properties[item].title}</th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.tableReducer.map((item, index) => (
+            <tr key={index}>
+              {schema.propertiesOrder.map((key, i) => {
+                const data = item[key];
+
+                if (key === 'id') {
+                  return null;
+                }
+                if (typeof data === 'object') {
+                  return (
+                    <td key={i}>{JSON.stringify(data)}</td>
+                  );
+                }
+                if (key === 'name') {
+                  return (
+                    <td key={i}>
+                      <Link to={'/' + this.props.schema.singular + '/' + item.id}>{data}</Link>
+                    </td>
+                  );
+                }
+                return (
+                  <td key={i}>{data}</td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
 Table.contextTypes = {
-  router: React.PropTypes.object
+  router: PropTypes.object
 };
 
-function mapStateToProps() {
+function mapStateToProps(state) {
   return {
+    tableReducer: state.tableReducer
   };
 }
 
 Table.propTypes = {
-  schema: React.PropTypes.object.isRequired
+  schema: PropTypes.object.isRequired,
+  tableReducer: PropTypes.array,
+  fetchData: PropTypes.func.isRequired
 };
+
 export default connect(mapStateToProps, {
+  fetchData,
+  clearData
 })(Table);
