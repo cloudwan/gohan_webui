9d2aaee47dfa09dceedb9f14fdcc1b965d447525
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-09-02 08:20:47 +0200

    Added Detail component with reducer and actions.

--- src/detail/Detail.js
+++ src/detail/Detail.js
@@ -1,26 +1,64 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 
+import {fetchData, clearData} from './DetailActions';
 
 class Detail extends Component {
+
+  componentWillMount() {
+    const {schema, splat} = this.props;
+
+    this.props.fetchData(schema.url + '/' + splat[splat.length - 1], schema.singular);
+  }
+
+  componentWillUnmount() {
+    this.props.clearData();
+  }
+
   render() {
-    console.log(this.props);
+    const {schema} = this.props.schema;
+    const {detailReducer} = this.props;
+
     return (
       <div>
-        Detail View
+        {schema.propertiesOrder.map((key, index) => {
+          const property = schema.properties[key];
+          const data = detailReducer[key];
+
+          if (property.view && !property.view.includes('detail')) {
+            return null;
+          }
+
+          return (
+            <div key={index}>
+              <div>{property.title}</div>
+              <div>{typeof data === 'object' ? JSON.stringify(data) : data}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 Detail.contextTypes = {
-  router: React.PropTypes.object
+  router: PropTypes.object
 };
 
-function mapStateToProps() {
+Detail.propTypes = {
+  schema: PropTypes.object.isRequired,
+  detailReducer: PropTypes.object,
+  fetchData: PropTypes.func.isRequired,
+  clearData: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
   return {
+    detailReducer: state.detailReducer
   };
 }
 
 export default connect(mapStateToProps, {
+  fetchData,
+  clearData
 })(Detail);
