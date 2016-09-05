1e5abf4c82c514e346f3d5bfe5fa8d2b5a430c7b
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-09-05 11:19:50 +0200

    Added Detail component with reducer and actions. (#104)

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -4,11 +4,13 @@ import errorReducer from './../error/errorReducer';
 import authReducer from './../auth/authReducer';
 import schemaReducer from './../schema/schemaReducer';
 import tableReducer from './../table/tableReducer';
+import detailReducer from './../detail/detailReducer';
 
 export default combineReducers({
   configReducer,
   errorReducer,
   authReducer,
   schemaReducer,
-  tableReducer
+  tableReducer,
+  detailReducer
 });
