419c6cde57a9a58c831ee5141685ef429e5154cc
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-09-06 12:07:03 +0200

    Modal table (#106)
    * Added "add new" modal and dialog component with reducer.
    
    * Added loading state to table component.

--- src/table/tableReducer.js
+++ src/table/tableReducer.js
@@ -1,13 +1,25 @@
-import {FETCH_SUCCESS, CLEAR_DATA} from './TableActionTypes';
+import {FETCH_SUCCESS, CLEAR_DATA, CREATE_SUCCESS} from './TableActionTypes';
 
-export default function configReducer(state = [], action): Object {
+export default function tableReducer(state = {isLoading: true, data: []}, action) {
+  const {data} = action;
+
   switch (action.type) {
     case FETCH_SUCCESS:
-      return [
-        ...action.data
-      ];
+      return {
+        ...state,
+        isLoading: false,
+        data
+      };
+    case CREATE_SUCCESS:
+      return {
+        ...state,
+        data: [...state.data, data]
+      };
     case CLEAR_DATA:
-      return [];
+      return {
+        isLoading: true,
+        data: []
+      };
     default:
       return state;
   }
