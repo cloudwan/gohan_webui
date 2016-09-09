68eb243ca40420e4c988245724ec495535fce4c6
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-09-09 13:11:44 +0200

    Improve DynamicRoutes and add child resource view.

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -3,16 +3,14 @@ import configReducer from './../config/configReducer';
 import errorReducer from './../error/errorReducer';
 import authReducer from './../auth/authReducer';
 import schemaReducer from './../schema/schemaReducer';
-import tableReducer from './../table/tableReducer';
-import detailReducer from './../detail/detailReducer';
 import dialogReducer from './../dialog/dialogReducer';
+import dynamicReducer from './../dynamicRoutes/dynamicReducer';
 
 export default combineReducers({
   configReducer,
   errorReducer,
   authReducer,
   schemaReducer,
-  tableReducer,
-  detailReducer,
-  dialogReducer
+  dialogReducer,
+  dynamicReducer
 });
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router';
 
 import App from './App';
-import dynamicRoutes from './dynamicRoutes';
+import dynamicRoutes from '../dynamicRoutes/dynamicRoutes';
 import requireAuthentication from '../auth/requireAuthentication';
 import Login from '../auth/Auth';
 
