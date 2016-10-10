e93db765da0693ec77cf28854b74d581cd667c3f
Adam Baszak <adambaszak@gmail.com>
2016-10-10 17:18:51 +0200

    Added Sidebar Menu Component.

--- src/app/components/SidebarMenu.jsx
+++ src/app/components/SidebarMenu.jsx
@@ -1,13 +1,52 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {Drawer, FontIcon, Divider} from 'material-ui';
+import {Link} from 'react-router';
+import SidebarMenuItem from '../components/SidebarMenuItem';
 
+const iconStyles = {
+  marginRight: 24
+};
+const drawerStyles = {
+  width: 280
+};
 export default class SidebarMenu extends Component {
   render() {
     const {children} = this.props;
+    const sidebarMenuItems = [];
+
+    if (this.props.schemaReducer !== undefined) {
+      this.props.schemaReducer.forEach((item, index) => {
+        if (!item.parent && item.metadata.type !== 'metaschema') {
+          sidebarMenuItems.push(
+            <SidebarMenuItem item={item}
+              key={index}
+            />
+          );
+        }
+      });
+    }
 
     return (
-      <ul>
+      <div>
+        <Drawer width={drawerStyles.width}>
+          <div>
+            <Link to={''}>
+              <h2>Gohan icon place</h2>
+            </Link>
+          </div>
+          <div>
+            <FontIcon className="material-icons" style={iconStyles}>home icon</FontIcon>
+          </div>
+          <Divider/>
+          {sidebarMenuItems}
+        </Drawer>
         {children}
-      </ul>
+      </div>
     );
   }
 }
+
+SidebarMenu.propTypes = {
+  schemaReducer: PropTypes.array
+};
+
