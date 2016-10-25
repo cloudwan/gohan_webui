b91070a022f2585aa75e0cc9f698cacfc99a39c5
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-10-25 13:26:22 +0200

    Added material ui loader to table.

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,7 +1,18 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
+import {Paper, RefreshIndicator} from 'material-ui';
+
 import Dialog from './../dialog/Dialog';
+
+const detailStyle = {
+  padding: 15
+};
+
+const loadingIndicatorStyle = {
+  margin: 'auto',
+  position: 'relative'
+};
 
 class Table extends Component {
 
@@ -35,11 +46,14 @@ class Table extends Component {
 
     if (this.props.isLoading || !Array.isArray(this.props.data)) {
       return (
-        <div>Loading...</div>
+        <RefreshIndicator size={60} left={0}
+          top={0} status="loading"
+          style={loadingIndicatorStyle}
+        />
       );
     }
     return (
-      <div>
+      <Paper style={detailStyle}>
         <button onClick={this.handleOpenModal}>{'Add new ' + singular}</button>
         <Dialog isOpen={this.state.openModal} action={this.state.actionModal}
           onRequestClose={this.handleCloseModal} schema={this.props.schema}
@@ -50,7 +64,7 @@ class Table extends Component {
           <thead>
             <tr>
               {schema.propertiesOrder.map((item, index) => {
-                if (item === 'id') {
+                if (item === 'id' || item === 'tenant_id') {
                   return null;
                 }
                 return (
@@ -65,7 +79,7 @@ class Table extends Component {
                 {schema.propertiesOrder.map((key, i) => {
                   const data = item[key];
 
-                  if (key === 'id') {
+                  if (key === 'id' || key === 'tenant_id') {
                     return null;
                   }
                   if (typeof data === 'object') {
@@ -88,7 +102,7 @@ class Table extends Component {
             ))}
           </tbody>
         </table>
-      </div>
+      </Paper>
     );
   }
 }
