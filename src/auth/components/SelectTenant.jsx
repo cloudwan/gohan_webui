24499809c224ce2255a0e50353ea5f7e93ae3093
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-10-25 12:25:00 +0200

    Added material-ui to select tenant.

--- src/auth/components/SelectTenant.jsx
+++ src/auth/components/SelectTenant.jsx
@@ -1,22 +1,45 @@
 import React, {Component, PropTypes} from 'react';
+import {SelectField, MenuItem, RaisedButton} from 'material-ui';
 
 export default class SelectTenant extends Component {
+
+  constructor(params) {
+    super(params);
+
+    this.state = {
+      value: null
+    };
+  }
 
   handleSelectTenantSubmit = event => {
     event.preventDefault();
     event.stopPropagation();
 
-    this.props.onTenantSubmit(this.tenant.value);
+    this.props.onTenantSubmit(this.state.value);
   };
+
+  handleTenantChange = (event, index, value) => this.setState({value});
 
   render() {
     return (
       <form onSubmit={this.handleSelectTenantSubmit}>
-        <select ref={c => {this.tenant = c;}}>
-          {this.props.tenants.map((tenant, key) => <option value={tenant.name} key={key}>{tenant.name}</option>)}
-        </select>
+        <SelectField floatingLabelText="Select tenant" value={this.state.value}
+          onChange={this.handleTenantChange} fullWidth={true}>
+          {
+            this.props.tenants.map(
+              (tenant, key) => (
+                <MenuItem value={tenant.name} key={key}
+                  primaryText={tenant.name}
+                />
+              )
+            )
+          }
+        </SelectField>
         <br/>
-        <button>Select Tenant</button>
+        <RaisedButton primary={true} label="Select Tenant"
+          fullWidth={true} type={'submit'}
+          disabled={!this.state.value}
+        />
       </form>
     );
   }
