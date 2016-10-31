b9836d0d845c33efc45f20f4e3d7380095d64d85
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-10-31 11:38:00 +0100

    Added custom routes to router.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,36 @@
 import App from './../app/App';
+import components from './componentsList';
 import dynamicRoutes from '../dynamicRoutes/dynamicRoutes';
 import requireAuthentication from '../auth/requireAuthentication';
 import Login from '../auth/Auth';
 
-export const createRoutes = () => {
+export const createRoutes = store => {
+  const {routes} = store.getState().configReducer;
+  const prepareRoutes = item => {
+
+    if (Array.isArray(item.childRoutes)) {
+      return item.childRoutes.map(item => {
+        const childRoutes = prepareRoutes(item);
+        const component = components[item.componentName];
+
+        if (component === undefined) {
+          throw new Error('Cannot select component! Check config.json!');
+        }
+
+        const result = {
+          path: item.path,
+          component,
+        };
+
+        if (childRoutes) {
+          result.childRoutes = childRoutes;
+        }
+
+        return result;
+      });
+    }
+  };
+
   return [
     {
       path: '/login',
@@ -13,6 +40,7 @@ export const createRoutes = () => {
       path: '/',
       component: requireAuthentication(App),
       childRoutes: [
+        ...prepareRoutes(routes),
         {
           path: '*',
           component: dynamicRoutes()
