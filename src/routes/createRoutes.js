b79bbc2108e242cb17eadc4b15059cabfde829a8
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-11-08 13:54:15 +0100

    Added parsing schema to routes, removed dynamic routers.

--- src/routes/createRoutes.js
+++ src/routes/createRoutes.js
@@ -1,11 +1,32 @@
 import App from './../app/App';
+import TableView from '../tableView';
+import DetailView from '../detailView';
+import NotFound from '../notFoundView';
 import components from './componentsList';
-import dynamicRoutes from '../dynamicRoutes/dynamicRoutes';
 
 export const createRoutes = store => {
   const {routes} = store.getState().configReducer;
-  const prepareRoutes = item => {
+  const {data} = store.getState().schemaReducer;
 
+  const schemaRoutes = data.reduce((result, item) => {
+    const singular = {
+      path: /* item.prefix + '/' + */ item.singular + '/:id',
+      getComponent(nextState, cb) {
+        return cb(null, DetailView(store));
+      }
+    };
+    const plural = {
+      path: /* item.prefix + '/' + */ item.plural,
+      getComponent(nextState, cb) {
+        return cb(null, TableView(store));
+      }
+    };
+    result.push(singular, plural);
+
+    return result;
+  }, []);
+
+  const prepareRoutes = item => {
     if (Array.isArray(item.childRoutes)) {
       return item.childRoutes.map(item => {
         const childRoutes = prepareRoutes(item);
@@ -35,7 +56,11 @@ export const createRoutes = store => {
       component: App,
       childRoutes: [
         ...prepareRoutes(routes),
-        dynamicRoutes(store)
+        ...schemaRoutes,
+        {
+          path: '*',
+          component: NotFound
+        }
       ]
     }
   ];
