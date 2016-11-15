bd861a06744cdf4aebb5c52344322f70f9b206c9
Katarzyna Fokow <sealionkat@gmail.com>
2016-11-15 08:59:14 +0100

    [PoC] Added shortpolling on details.

--- src/detailView/DetailActions.js
+++ src/detailView/DetailActions.js
@@ -3,8 +3,14 @@ import {
   FETCH_SUCCESS,
   FETCH_CHILD_SUCCESS,
   FETCH_FAILURE,
-  CLEAR_DATA
+  CREATE_SUCCESS,
+  CREATE_FAILURE,
+  CLEAR_DATA,
+  POLLING_DATA,
+  CANCEL_POLLING_DATA
 } from './DetailActionTypes';
+
+let timeoutId = null;
 
 function fetchSuccess(data) {
   return dispatch => {
@@ -20,7 +26,7 @@ function fetchError(data) {
   };
 }
 
-export function fetchData(url, plural) {
+export function fetchData(url, plural, cancelToken, polling) {
   return (dispatch, getState) => {
     const state = getState();
     const headers = {
@@ -28,10 +34,20 @@ export function fetchData(url, plural) {
       'X-Auth-Token': state.authReducer.tokenId
     };
 
-    axios.get(state.configReducer.gohan.url + url, {headers}).then(response => {
+    axios.get(state.configReducer.gohan.url + url, {headers, cancelToken}).then(response => {
       dispatch(fetchSuccess(response.data[plural]));
+      if (polling) {
+        dispatch(pollData(url, plural, cancelToken));
+      }
     }).catch(error => {
-      dispatch(fetchError(error.response));
+      if (axios.isCancel(error)) {
+        clearTimeout(timeoutId);
+      } else {
+        dispatch(fetchError(error.response));
+        if (polling) {
+          dispatch(pollData(url, plural, cancelToken));
+        }
+      }
     });
   };
 }
@@ -59,8 +75,57 @@ export function fetchChildrenData(url) {
   };
 }
 
+
+function createSuccess(data) {
+  return dispatch => {
+    dispatch({data, type: CREATE_SUCCESS});
+  };
+}
+
+function createError(data) {
+  return dispatch => {
+    const error = data.data;
+
+    dispatch({type: CREATE_FAILURE, error});
+  };
+}
+
+
+export function createData(url, data, singular) {
+  return (dispatch, getState) => {
+    const state = getState();
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Auth-Token': state.authReducer.tokenId
+    };
+
+    axios.post(state.configReducer.gohan.url + url, data, {headers}).then(response => {
+      dispatch(createSuccess(response.data[singular]));
+    }).catch(error => {
+      dispatch(createError(error.response));
+    });
+  };
+}
+
 export function clearData() {
   return dispatch => {
     dispatch({type: CLEAR_DATA});
+  };
+}
+
+export function pollData(url, quantity, sourceToken) {
+  return dispatch => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      dispatch({type: POLLING_DATA});
+      dispatch(fetchData(url, quantity, sourceToken, true));
+    }, 10000);
+  };
+}
+
+export function cancelPollData() {
+  return dispatch => {
+    clearTimeout(timeoutId);
+    dispatch({type: CANCEL_POLLING_DATA});
   };
 }
