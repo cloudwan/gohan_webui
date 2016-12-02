90e1d63bc990c303a59a36db45a86c0dacfbf2d2
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-12-02 13:26:37 +0100

    Configured tests and added sample test for configReducer and NotFound view.

--- src/config/configReducer.js
+++ src/config/configReducer.js
@@ -1,19 +1,8 @@
-/* global location */
 import {FETCH_SUCCESS} from './ConfigActionTypes';
 
-export default function configReducer(state = {}, action): Object {
+export default function configReducer(state = {}, action) {
   switch (action.type) {
     case FETCH_SUCCESS:
-      if (action.data.authUrl.includes('__HOST__')) {
-        action.data.authUrl = action.data.authUrl.replace(
-        '__HOST__', location.hostname);
-      }
-
-      if (action.data.gohan.url.includes('__HOST__')) {
-        action.data.gohan.url = action.data.gohan.url.replace(
-          '__HOST__', location.hostname);
-      }
-
       return {
         ...state,
         ...action.data
---
+++ src/notFoundView/NotFound.test.jsx
@@ -0,0 +1,18 @@
+/* global it, describe */
+import React from 'react';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {shallow} from 'enzyme';
+
+import NotFound from './index';
+
+chai.use(chaiEnzyme());
+chai.should();
+
+describe('NotFound', function () {
+  it('should exist', () => {
+    const wrapper = shallow(<NotFound/>);
+
+    wrapper.should.not.equal(undefined);
+  });
+});
