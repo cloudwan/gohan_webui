9844f0eb856ef1363ce97f8b7a8fc85ed08a4f45
Katarzyna Fokow <sealionkat@gmail.com>
2016-12-05 17:11:16 +0100

    [PoC] Refactoring of table pagination.
    [PoC] Refactoring of refresh indicator.
    
    [PoC] Refactoring of table toolbar.

---
+++ src/components/Table/TablePaginationComponent.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import PaginationComponent from 'react-ultimate-pagination-material-ui';
+
+class TablePaginationComponent extends Component {
+  render() {
+    let pagination = null;
+
+    if (this.props.pageCount > 1) {
+      pagination = <PaginationComponent currentPage={this.props.activePage} totalPages={this.props.pageCount}
+        onChange={this.props.handlePageClick}
+      />;
+    }
+
+    return (
+      <div>
+        {pagination}
+      </div>
+    );
+
+  }
+
+}
+
+export default TablePaginationComponent;
