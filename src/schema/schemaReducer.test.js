bc0bec3e1212bfd80dcf487e33db9873c1b49b58
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-12-06 09:11:50 +0100

    Added tests for schema reducer and schema actions.
    Fixed small issues.

---
+++ src/schema/schemaReducer.test.js
@@ -0,0 +1,42 @@
+/* global it, describe */
+import chai from 'chai';
+import * as actionTypes from './SchemaActionTypes';
+import schemaReducer from './schemaReducer';
+
+chai.should();
+
+describe('schemaReducer ', () => {
+
+  it('should return initial state', () => {
+    schemaReducer(undefined, {}).should.deep.equal({
+      data: [],
+      isLoading: true
+    });
+  });
+
+  it('should handle FETCH_SUCCESS', () => {
+    schemaReducer(
+      undefined, {
+        type: actionTypes.FETCH_SUCCESS,
+        data: [
+          {
+            path: 'sample1'
+          },
+          {
+            path: 'sample2'
+          }
+        ]
+      }
+    ).should.deep.equal({
+      data: [
+        {
+          path: 'sample1'
+        },
+        {
+          path: 'sample2'
+        }
+      ],
+      isLoading: false
+    });
+  });
+});
