e9b41f93d4a8daa8c7ef14b351be4e5c45c341ad
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-01-04 15:11:11 +0100

    Moved import css to main.scss file.

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ import {updateLocation} from './location/LocationActions';
 
 import '../css/sass/main.scss';
 
-import '@blueprintjs/core/dist/blueprint.css';
-
 const store = createStore(window.devToolsExtension && window.devToolsExtension());
 store.unsubscribeHistory = hashHistory.listen(updateLocation(store));
 
