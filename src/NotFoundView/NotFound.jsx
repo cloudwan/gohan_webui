36cccc5af76fff5144818e669351ce8b98d8a579
Adam Baszak <adambaszak@gmail.com>
2017-01-05 13:13:27 +0100

    Apply blueprint to NotFound View.

--- src/NotFoundView/NotFound.jsx
+++ src/NotFoundView/NotFound.jsx
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
-import {Paper} from 'material-ui';
-
-const detailStyle = {
-  padding: 15
-};
 
 export default class NotFound extends Component {
   render() {
     return (
-      <Paper style={detailStyle}>
-        <p style={{fontWeight: 'bold'}}>Occurred, a 404 error has...</p>
+      <div className="pt-card pt-elevation-3">
+        <h2>Occurred, a 404 error has...</h2>
         <p>Lost a page i have. How embarrassing...</p>
-      </Paper>
+      </div>
     );
   }
 }
