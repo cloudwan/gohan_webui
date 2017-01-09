c1053aeaa2720b024b947c57289db4156094b3fe
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-01-09 12:58:12 +0100

    Blueprint.js LoadingIndicator component.

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
-import {RefreshIndicator} from 'material-ui';
-
-const loadingIndicatorStyle = {
-  margin: 'auto',
-  position: 'relative'
-};
+import {Spinner, Intent} from '@blueprintjs/core';
 
 export default class LoadingIndicator extends Component {
   render() {
     return (
-      <RefreshIndicator size={60} left={0}
-        top={0} status="loading"
-        style={loadingIndicatorStyle}
-      />
+      <Spinner className="pt-large loading-spinner" intent={Intent.PRIMARY}/>
     );
   }
 }
