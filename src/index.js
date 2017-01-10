48c9e6e9f14e026656b9abc177b980e57976dec6
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-01-10 11:36:10 +0100

    Changed pagination component without material ui.

--- src/index.js
+++ src/index.js
@@ -2,50 +2,26 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {hashHistory} from 'react-router';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import '../css/sass/main.scss';
 
 import Root from './app/Root';
 import createStore from './app/store';
 import {fetchConfig} from './config/ConfigActions';
-
 import {updateLocation} from './location/LocationActions';
 
-import '../css/sass/main.scss';
 
 const store = createStore(window.devToolsExtension && window.devToolsExtension());
 store.unsubscribeHistory = hashHistory.listen(updateLocation(store));
 
-const muiTheme = getMuiTheme({
-  palette: {
-  },
-  appBar: {
-    height: 50
-  },
-  zIndex: {
-    menu: 1000,
-    appBar: 1100,
-    leftNavOverlay: 1200,
-    leftNav: 1300,
-    dialogOverlay: 1400,
-    dialog: 1500,
-    layer: 2000,
-    popover: 2100,
-    snackbar: 2900,
-    tooltip: 3000
-  }
-});
-
 console.log(`Gohan version: ${VERSION.gohanWebUI.version}, repo tag: ${VERSION.gohanWebUI.tag}`);
 
-injectTapEventPlugin();
 store.dispatch(fetchConfig()).then(() => {
 
   render(
-    <MuiThemeProvider muiTheme={muiTheme}>
+    (
       <Root store={store} history={hashHistory} />
-    </MuiThemeProvider>,
+    ),
     document.getElementById('root')
   );
 });
