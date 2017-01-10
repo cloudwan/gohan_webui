52078398475429f8374a02667cf09f0fef95c5b3
Adam Baszak <adambaszak@gmail.com>
2017-01-10 19:53:39 +0100

    Added sorting to Table Component.

--- src/components/Table/TableHeaderComponent.jsx
+++ src/components/Table/TableHeaderComponent.jsx
@@ -1,15 +1,56 @@
 import React, {Component, PropTypes} from 'react';
+import {Tooltip, Position} from '@blueprintjs/core';
 
 class TableHeader extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortKey: this.props.sortKey || '',
+      sortOrder: this.props.sortOrder || ''
+    };
+  }
+
+  handleHeaderClick = event => {
+    let sortKey = event.currentTarget.innerText.toLowerCase();
+    let sortOrder = 'asc';
+
+    if (sortKey === this.state.sortKey && this.state.sortOrder === 'asc') {
+      sortOrder = 'desc';
+    } else if (sortKey === this.state.sortKey && this.state.sortOrder === 'desc') {
+      sortKey = '';
+    }
+
+    this.setState({sortKey, sortOrder});
+
+    this.props.sortData(sortKey, sortOrder);
+  };
+
   buildTableHeaders = () => {
     const {properties, visibleColumns} = this.props;
 
     return visibleColumns.map((item, index) => {
       const property = properties[item];
+      let sortIcon = <Tooltip content="Sort by this property." position={Position.RIGHT}>
+        <span className={'hidden pt-icon-small pt-icon-sort'}/>
+      </Tooltip>;
+
+      if (this.state.sortKey === property.title.toLowerCase()) {
+        sortIcon = <Tooltip content={this.state.sortOrder === 'asc' ?
+          'Sorted in ascending order.' :
+          'Sorted in descending order.'}
+          position={Position.RIGHT}>
+          <span className={'pt-icon-small pt-icon-sort-' + this.state.sortOrder}/>
+        </Tooltip>;
+      }
 
       return (
         <th key={index}>
-          {property.title}
+          <a onClick={this.handleHeaderClick}>
+            <Tooltip content={property.description}><span>{property.title}</span></Tooltip>
+            {sortIcon}
+          </a>
         </th>
       );
     });
@@ -19,7 +60,7 @@ class TableHeader extends Component {
     return (
       <tr>
         {this.buildTableHeaders()}
-        <th >{'Options'}</th>
+        <th className="options">{'Options'}</th>
       </tr>
     );
   }
