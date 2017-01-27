8482a65ace3669fba2a2756ad96e501e381b6925
Katarzyna Fokow <sealionkat@gmail.com>
2017-01-27 11:23:54 +0100

    Added basic tests for some widgets.

---
+++ src/Dialog/formComponents/widgets/Ipv4Widget.test.js
@@ -0,0 +1,31 @@
+/* global it, describe */
+import React from 'react';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import spies from 'chai-spies';
+import {shallow} from 'enzyme';
+
+import Ipv4Widget from './Ipv4Widget';
+import BaseInput from './BaseInput';
+
+chai.use(spies);
+chai.use(chaiEnzyme());
+chai.should();
+
+describe('< Ipv4Widget />', function () {
+  it('should exist', () => {
+    const wrapper = shallow(
+      <Ipv4Widget id={'0'} />
+    );
+
+    wrapper.should.not.equal(undefined);
+  });
+
+  it('should contain 1 BaseInput', () => {
+    const wrapper = shallow(
+      <Ipv4Widget id={'0'} />
+    );
+
+    wrapper.find(BaseInput).should.have.length(1);
+  });
+});
---
+++ src/Dialog/formComponents/widgets/TextWidget.test.js
@@ -0,0 +1,31 @@
+/* global it, describe */
+import React from 'react';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import spies from 'chai-spies';
+import {shallow} from 'enzyme';
+
+import TextWidget from './TextWidget';
+import BaseInput from './BaseInput';
+
+chai.use(spies);
+chai.use(chaiEnzyme());
+chai.should();
+
+describe('< TextWidget />', function () {
+  it('should exist', () => {
+    const wrapper = shallow(
+      <TextWidget id={'0'} />
+    );
+
+    wrapper.should.not.equal(undefined);
+  });
+
+  it('should contain 1 BaseInput', () => {
+    const wrapper = shallow(
+      <TextWidget id={'0'} />
+    );
+
+    wrapper.find(BaseInput).should.have.length(1);
+  });
+});
