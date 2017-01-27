99f950a54f46f4591e772469b93035b1dfa7e55d
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-01-27 14:55:19 +0100

    Fix problem with error after change page.
    ESI-10461

--- src/TableView/index.js
+++ src/TableView/index.js
@@ -2,8 +2,52 @@ import TableView from './TableView';
 import {injectReducer} from '../app/reducers';
 import reducer from './tableReducer';
 
-export default store => {
-  injectReducer(store, {key: 'tableReducer', reducer});
+import {initialize, fetchData, clearData} from './TableActions';
 
-  return TableView;
-};
+export default TableView;
+
+export function onTableEnter(store, plural, nextState) {
+  injectReducer(store, {key: 'tableReducer', reducer});
+  const state = store.getState();
+  const activeSchema = state.schemaReducer.data.find(
+    object => object.plural === plural
+  );
+  let filters;
+
+  if (nextState.location.query.filters) {
+    try {
+      filters = JSON.parse(nextState.location.query.filters);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const {
+    sortKey,
+    sortOrder,
+    limit = 0,
+    offset = 0,
+  } = nextState.location.query;
+
+  store.dispatch(
+    initialize(
+      activeSchema.url, plural,
+      {
+        sortKey,
+        sortOrder,
+        limit,
+        offset,
+        filters
+      }
+    )
+  );
+
+  store.dispatch(
+    fetchData(plural)
+  );
+}
+
+
+export function onTableLeave(store, plural) {
+  store.dispatch(clearData(plural));
+}
