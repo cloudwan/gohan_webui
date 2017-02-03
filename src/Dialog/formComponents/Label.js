3d19df83b55d3f2e7d3c8b52128c7207880bc98d
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-02-03 12:51:53 +0100

    Clean up dialog. Updated react-jsonschema-form lib.

--- src/Dialog/formComponents/Label.js
+++ src/Dialog/formComponents/Label.js
@@ -4,13 +4,16 @@ const REQUIRED_FIELD_SYMBOL = '*';
 
 function Label(props) {
   const {label, required, id} = props;
+
   if (!label) {
     return <div />;
   }
+
   return (
-    <label className='control-label' htmlFor={id}>
-      {required ? label + REQUIRED_FIELD_SYMBOL : label}
-    </label>
+    <legend id={id}>
+      {label}
+      {required ? <span className={'form-asterisk'}>{REQUIRED_FIELD_SYMBOL}</span> : null}
+    </legend>
   );
 }
 
