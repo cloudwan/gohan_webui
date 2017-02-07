8e0ad4cb1559983a20cc0739fd552056eaf82466
Katarzyna Fokow <sealionkat@gmail.com>
2017-02-07 14:03:55 +0100

    Added search in sidebar and updated tests.
    ESI-10799

--- src/app/components/SidebarMenu.jsx
+++ src/app/components/SidebarMenu.jsx
@@ -2,28 +2,60 @@ import React, {Component, PropTypes} from 'react';
 import {Menu, MenuItem} from '@blueprintjs/core';
 
 export default class SidebarMenu extends Component {
-  render() {
-    const sidebarMenuItems = [];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchQuery: ''
+    };
+  }
+
+  handleSearchChange = event => {
+    const searchQuery = event.target.value.replace(/[\(\)\[\]]/g, '\\$&');
+
+    this.setState({searchQuery});
+  };
+
+  buildMenuItems() {
     if (this.props.schemaReducer !== undefined) {
-      this.props.schemaReducer.data.forEach((item, index) => {
+      const {data} = this.props.schemaReducer;
+      const {searchQuery} = this.state;
+      const searchQueryRE = new RegExp(searchQuery, 'i');
+      const menuItems = [];
+
+      data.forEach((item, index) => {
         if (!item.parent && item.metadata.type !== 'metaschema') {
-          sidebarMenuItems.push(
-            <MenuItem item={item}
-              key={index}
-              text={item.title}
-              href={'#/' + item.plural}
-              className="item"
-            />
-          );
+          if (searchQuery === '' || searchQueryRE.test(item.title)) {
+            menuItems.push(
+              <MenuItem item={item}
+                key={index}
+                text={item.title}
+                href={'#/' + item.plural}
+                className="item"
+              />
+            );
+          }
         }
       });
-    }
 
+      return menuItems;
+    }
+  }
+
+  render() {
     return (
-      <Menu className="pt-menu pt-large pt-elevation-2 pt-fixed-top sidebar">
-        {sidebarMenuItems}
-      </Menu>
+      <div className="pt-elevation-2 pt-fixed-top sidebar">
+        <div className="sidebar-search">
+          <label className="pt-label">
+            <input type="text" className="pt-input"
+              placeholder="Search" onChange={this.handleSearchChange}
+            />
+          </label>
+        </div>
+        <Menu className="pt-menu pt-large">
+          {this.buildMenuItems()}
+        </Menu>
+      </div>
     );
   }
 }
@@ -31,4 +63,3 @@ export default class SidebarMenu extends Component {
 SidebarMenu.propTypes = {
   schemaReducer: PropTypes.object
 };
-
--- src/app/components/SidebarMenu.test.js
+++ src/app/components/SidebarMenu.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import spies from 'chai-spies';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import {Menu, MenuItem} from '@blueprintjs/core';
 import SidebarMenu from './SidebarMenu';
@@ -19,6 +19,22 @@ describe('< SidebarMenu />', function () {
     );
 
     wrapper.should.not.equal(undefined);
+  });
+
+  it('should contain 2 divs', () => {
+    const wrapper = shallow(
+      <SidebarMenu/>
+    );
+
+    wrapper.find('div').should.have.length(2);
+  });
+
+  it('should contain 1 label', () => {
+    const wrapper = shallow(
+      <SidebarMenu/>
+    );
+
+    wrapper.find('label').should.have.length(1);
   });
 
   it('should contain 1 Menu', () => {
@@ -48,10 +64,11 @@ describe('< SidebarMenu />', function () {
         }
       ]
     };
-    const wrapper = shallow(
+    const wrapper = mount(
       <SidebarMenu schemaReducer={schemaReducer}/>
     );
 
+    wrapper.update();
     wrapper.find(MenuItem).should.have.length(2);
   });
 
@@ -81,10 +98,138 @@ describe('< SidebarMenu />', function () {
         }
       ]
     };
-    const wrapper = shallow(
+    const wrapper = mount(
       <SidebarMenu schemaReducer={schemaReducer}/>
     );
 
+    wrapper.update();
     wrapper.find(MenuItem).should.have.length(2);
+  });
+
+  it('should have initial state', () => {
+    const wrapper = shallow(
+      <SidebarMenu/>
+    );
+
+    wrapper.should.have.state('searchQuery');
+    wrapper.state('searchQuery').should.be.equal('');
+  });
+
+  it('should call onChange when changed input value', () => {
+    const onChange = chai.spy();
+    const wrapper = mount(
+      <SidebarMenu/>
+    );
+
+    wrapper.find('[type="text"]').get(0).onChange = onChange;
+    wrapper.find('[type="text"]').simulate('change', {target: {value: 'a'}});
+  });
+
+  it('should have initial menu items when search string is empty', () => {
+    const schemaReducer = {
+      data: [
+        {
+          title: 'title1',
+          plural: 'plural1',
+          metadata: {
+            type: 'type1'
+          }
+        },
+        {
+          title: 'title2',
+          plural: 'plural2',
+          metadata: {
+            type: 'type2'
+          }
+        },
+        {
+          title: 'title3',
+          plural: 'plural3',
+          metadata: {
+            type: 'metaschema'
+          }
+        }
+      ]
+    };
+    const wrapper = mount(
+      <SidebarMenu schemaReducer={schemaReducer}/>
+    );
+
+    wrapper.update();
+    wrapper.find('[type="text"]').simulate('change', {target: {value: ''}});
+
+    wrapper.find(MenuItem).should.have.length(2);
+  });
+
+  it('should have filtered menu items when search string is not empty', () => {
+    const schemaReducer = {
+      data: [
+        {
+          title: 'title1',
+          plural: 'plural1',
+          metadata: {
+            type: 'type1'
+          }
+        },
+        {
+          title: 'title2',
+          plural: 'plural2',
+          metadata: {
+            type: 'type2'
+          }
+        },
+        {
+          title: 'title3',
+          plural: 'plural3',
+          metadata: {
+            type: 'metaschema'
+          }
+        }
+      ]
+    };
+    const wrapper = mount(
+      <SidebarMenu schemaReducer={schemaReducer}/>
+    );
+
+    wrapper.update();
+    wrapper.find('[type="text"]').simulate('change', {target: {value: 'title2'}});
+
+    wrapper.find(MenuItem).should.have.length(1);
+  });
+
+  it('should have 0 menu items when search string does not match any item', () => {
+    const schemaReducer = {
+      data: [
+        {
+          title: 'title1',
+          plural: 'plural1',
+          metadata: {
+            type: 'type1'
+          }
+        },
+        {
+          title: 'title2',
+          plural: 'plural2',
+          metadata: {
+            type: 'type2'
+          }
+        },
+        {
+          title: 'title3',
+          plural: 'plural3',
+          metadata: {
+            type: 'metaschema'
+          }
+        }
+      ]
+    };
+    const wrapper = mount(
+      <SidebarMenu schemaReducer={schemaReducer}/>
+    );
+
+    wrapper.update();
+    wrapper.find('[type="text"]').simulate('change', {target: {value: 'title4'}});
+
+    wrapper.find(MenuItem).should.have.length(0);
   });
 });
