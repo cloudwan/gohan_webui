6a4767bf29c97bf95005fb3d93d34ed57266dd73
Katarzyna Fokow <sealionkat@gmail.com>
2017-02-09 13:31:46 +0100

    Added BooleanField tests.

--- src/Dialog/formComponents/fields/BooleanField.test.js
+++ src/Dialog/formComponents/fields/BooleanField.test.js
@@ -2,16 +2,16 @@
 import React from 'react';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
-import spies from 'chai-spies';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
+import BaseInput from '../widgets/BaseInput';
+import CheckboxWidget from '../widgets/CheckboxWidget';
 import BooleanField from './BooleanField';
 
-chai.use(spies);
 chai.use(chaiEnzyme());
 chai.should();
 
-describe('< BooleanField />', function () {
+describe('< BooleanField />', () => {
   it('should exist', () => {
     const wrapper = shallow(
       <BooleanField schema={{}} onChange={() => {}}
@@ -20,6 +20,31 @@ describe('< BooleanField />', function () {
     );
 
     wrapper.should.not.equal(undefined);
+  });
+
+  it('should contain 1 CheckboxWidget on default', () => {
+    const wrapper = shallow(
+      <BooleanField schema={{}} onChange={() => {}}
+        idSchema={{$id: '0'}}
+      />
+    );
+
+    wrapper.find(CheckboxWidget).should.have.length(1);
+  });
+
+
+  it('should contain 1 BaseInput widget when passed ui:widget', () => {
+    const uiSchema = {
+      'ui:widget': BaseInput
+    };
+    const wrapper = mount(
+      <BooleanField schema={{}} uiSchema={uiSchema}
+        onChange={() => {}} onBlur={() => {}}
+        formContext={{}}
+      />
+    );
+
+    wrapper.find(BaseInput).should.have.length(1);
   });
 
 });
