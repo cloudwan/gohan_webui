a53b4e6aa26debc8c05a1e3af5a1a0a596302f08
Katarzyna Fokow <sealionkat@gmail.com>
2017-02-17 13:44:50 +0100

    Fixed some tests of Dialog.

--- src/Dialog/Dialog.test.js
+++ src/Dialog/Dialog.test.js
@@ -4,7 +4,7 @@ import configureStore from 'redux-mock-store';
 import chai from 'chai';
 import spies from 'chai-spies';
 import chaiEnzyme from 'chai-enzyme';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import {Dialog, Button, ProgressBar} from '@blueprintjs/core';
 import Form from 'react-jsonschema-form';
@@ -17,18 +17,48 @@ chai.should();
 const mockStore = configureStore();
 
 describe('< Dialog />', () => {
-  const action = ['title'];
-  const schema = {
+  const action = 'create';
+  const baseSchema = {
     singular: 'singular',
+    schema: {
+      propertiesOrder: ['test1', 'test2'],
+      properties: {
+        test1: {
+          type: 'string'
+        },
+        test2: {
+          type: 'string'
+        }
+      }
+    },
+    parent: ''
   };
   const data = {
     test1: 'test1',
     test2: 'test2',
     id: 0
   };
+  const schema = {
+    propertiesOrder: ['test1', 'test2'],
+    properties: {
+      test1: {
+        type: 'string'
+      },
+      test2: {
+        type: 'string'
+      }
+    }
+  };
 
   it('should exist', () => {
-    const store = mockStore({});
+    const store = mockStore({
+      dialogReducer: {
+        schema: {
+          propertiesOrder: ['test1', 'test2']
+        },
+        isLoading: false
+      }
+    });
     const wrapper = shallow(
       <ConnectedDialog store={store} />
     );
@@ -37,56 +67,42 @@ describe('< Dialog />', () => {
   });
 
   it('should contain particular elements', () => {
-    const dialogReducer = {
-      isLoading: false,
-      schema: {
-        propertiesOrder: ['test1', 'test2']
-      }
-    };
     const wrapper = shallow(
-      <GeneratedDialog action={action} schema={schema}
-        dialogReducer={dialogReducer} data={data}
+      <GeneratedDialog action={action} baseSchema={baseSchema}
+        schema={schema} data={data}
+        prepareSchema={() => {}} isLoading={false}
+        clearData={() => {}} isOpen={true}
       />
     );
 
     wrapper.find(Dialog).should.have.length(1);
     wrapper.find(Form).should.have.length(1);
     wrapper.find('div').should.have.length(5);
+    wrapper.find(Button).should.have.length(1);
+  });
+
+  it('should contain 2 Buttons when passed onClose function', () => {
+    const wrapper = shallow(
+      <GeneratedDialog action={action} baseSchema={baseSchema}
+        schema={schema} data={data}
+        prepareSchema={() => {}} isLoading={false}
+        onClose={() => {}} clearData={() => {}}
+        isOpen={true}
+      />
+      );
+
     wrapper.find(Button).should.have.length(2);
   });
 
   it('should contain ProgressBar when loading', () => {
-    const dialogReducer = {
-      isLoading: true,
-      schema: {
-        propertiesOrder: ['test1', 'test2']
-      }
-    };
     const wrapper = shallow(
-      <GeneratedDialog action={action} schema={schema}
-        dialogReducer={dialogReducer} data={data}
+      <GeneratedDialog action={action} baseSchema={baseSchema}
+        schema={schema} data={data}
+        prepareSchema={() => {}} isLoading={true}
+        clearData={() => {}} isOpen={true}
       />
     );
 
     wrapper.find(ProgressBar).should.have.length(1);
-  });
-
-  it('should call clearData on unmounting', () => {
-    const clearData = chai.spy();
-    const dialogReducer = {
-      isLoading: false,
-      schema: {
-        propertiesOrder: ['test1', 'test2']
-      }
-    };
-    const wrapper = mount(
-      <GeneratedDialog action={action} schema={schema}
-        dialogReducer={dialogReducer} data={data}
-        fetchRelationFields={() => {}} clearData={clearData}
-      />
-    );
-
-    wrapper.unmount();
-    clearData.should.have.been.called.once; // eslint-disable-line no-unused-expressions
   });
 });
