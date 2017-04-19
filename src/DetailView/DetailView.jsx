93bba8194be2cd1c2ce483b063ef0780a1d62d1f
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-04-19 14:27:11 +0200

    Added hoc for detail view and table view.

--- src/DetailView/DetailView.jsx
+++ src/DetailView/DetailView.jsx
@@ -13,147 +13,150 @@ import {
 } from './DetailActions';
 import {Alert, Intent} from '@blueprintjs/core';
 
-class DetailView extends Component {
+export const getDetailView = (DetailComponent = Detail) => {
+  class DetailView extends Component {
+    constructor(props) {
+      super(props);
 
-  constructor(props) {
-    super(props);
+      const activeSchema = props.schemaReducer.data.find(
+        object => object.singular === this.props.singular
+      );
+      const childSchemas = props.schemaReducer.data.filter(
+        object => object.parent === activeSchema.id
+      );
+      const detail = props.params.id;
+      const CancelToken = axios.CancelToken;
+      const source = CancelToken.source();
 
-    const activeSchema = props.schemaReducer.data.find(
-      object => object.singular === this.props.singular
-    );
-    const childSchemas = props.schemaReducer.data.filter(
-      object => object.parent === activeSchema.id
-    );
-    const detail = props.params.id;
-    const CancelToken = axios.CancelToken;
-    const source = CancelToken.source();
+      this.state = {
+        activeSchema,
+        childSchemas,
+        detail,
+        source,
+        modalOpen: false,
+        alertOpen: false,
+        actionModal: 'update',
+        dialogData: {}
+      };
 
-    this.state = {
-      activeSchema,
-      childSchemas,
-      detail,
-      source,
-      modalOpen: false,
-      alertOpen: false,
-      actionModal: 'update',
-      dialogData: {}
+      props.initialize(activeSchema.url + '/' + detail, activeSchema.singular);
+
+      props.fetchData();
+    }
+
+    componentWillUnmount() {
+      this.props.clearData();
+    }
+
+    handleEdit = id => {
+      this.setState({modalOpen: true, actionModal: 'update', dialogData: id});
     };
 
-    props.initialize(activeSchema.url + '/' + detail, activeSchema.singular);
+    handleDelete = () => {
+      this.props.deleteData();
+      this.handleCloseAlert();
+    };
 
-    props.fetchData();
-  }
+    handleOpenModal = () => {
+      this.setState({modalOpen: true, actionModal: 'create', dialogData: {}});
+    };
 
-  componentWillUnmount() {
-    this.props.clearData();
-  }
+    handleCloseModal = () => {
+      this.setState({modalOpen: false});
+    };
 
-  handleEdit = id => {
-    this.setState({modalOpen: true, actionModal: 'update', dialogData: id});
-  };
+    handleOpenAlert = () => {
+      this.setState({alertOpen: true});
+    };
 
-  handleDelete = () => {
-    this.props.deleteData();
-    this.handleCloseAlert();
-  };
+    handleCloseAlert = () => {
+      this.setState({alertOpen: false});
+    };
 
-  handleOpenModal = () => {
-    this.setState({modalOpen: true, actionModal: 'create', dialogData: {}});
-  };
+    handleSubmit = (data) => {
+      switch (this.state.actionModal) {
+        case 'create':
+          this.props.createData(data);
+          break;
+        case 'update':
+          this.props.updateData(data);
+          break;
+      }
+    };
 
-  handleCloseModal = () => {
-    this.setState({modalOpen: false});
-  };
+    showModal = () => {
+      if (this.state.modalOpen) {
+        return (
+          <Dialog isOpen={this.state.modalOpen} action={this.state.actionModal}
+            data={this.state.dialogData} onClose={this.handleCloseModal}
+            onSubmit={this.handleSubmit} baseSchema={this.state.activeSchema}
+          />
+        );
+      }
 
-  handleOpenAlert = () => {
-    this.setState({alertOpen: true});
-  };
+      return null;
+    };
 
-  handleCloseAlert = () => {
-    this.setState({alertOpen: false});
-  };
+    showAlert = () => {
+      if (this.state.alertOpen) {
+        return (
+          <Alert intent={Intent.PRIMARY}
+            isOpen={this.state.alertOpen}
+            confirmButtonText='Delete'
+            cancelButtonText='Cancel'
+            onConfirm={this.handleDelete}
+            onCancel={this.handleCloseAlert}>
+            <p>Delete?</p>
+          </Alert>
+        );
+      }
+    };
 
-  handleSubmit = (data) => {
-    switch (this.state.actionModal) {
-      case 'create':
-        this.props.createData(data);
-        break;
-      case 'update':
-        this.props.updateData(data);
-        break;
-    }
-  };
+    render() {
+      const {data, isLoading} = this.props.detailReducer;
 
-  showModal = () => {
-    if (this.state.modalOpen) {
+      if (isLoading) {
+        return (
+          <LoadingIndicator/>
+        );
+      }
+
       return (
-        <Dialog isOpen={this.state.modalOpen} action={this.state.actionModal}
-          data={this.state.dialogData} onClose={this.handleCloseModal}
-          onSubmit={this.handleSubmit} baseSchema={this.state.activeSchema}
-        />
+        <div className="detail-container">
+          {this.showModal()}
+          {this.showAlert()}
+          <DetailComponent schema={this.state.activeSchema} data={data}
+            onEdit={this.handleEdit} onDelete={this.handleOpenAlert}
+          />
+        </div>
       );
     }
-
-    return null;
-  };
-
-  showAlert = () => {
-    if (this.state.alertOpen) {
-      return (
-        <Alert intent={Intent.PRIMARY}
-          isOpen={this.state.alertOpen}
-          confirmButtonText='Delete'
-          cancelButtonText='Cancel'
-          onConfirm={this.handleDelete}
-          onCancel={this.handleCloseAlert}>
-          <p>Delete?</p>
-        </Alert>
-      );
-    }
-  };
-
-  render() {
-    const {data, isLoading} = this.props.detailReducer;
-
-    if (isLoading) {
-      return (
-        <LoadingIndicator/>
-      );
-    }
-
-    return (
-      <div className="detail-container">
-        {this.showModal()}
-        {this.showAlert()}
-        <Detail schema={this.state.activeSchema} data={data}
-          onEdit={this.handleEdit} onDelete={this.handleOpenAlert}
-        />
-      </div>
-    );
   }
-}
 
-DetailView.propTypes = {
-  schemaReducer: PropTypes.object.isRequired,
-  detailReducer: PropTypes.object.isRequired,
-  configReducer: PropTypes.object.isRequired,
-  fetchData: PropTypes.func.isRequired,
-  clearData: PropTypes.func.isRequired,
+  DetailView.propTypes = {
+    schemaReducer: PropTypes.object.isRequired,
+    detailReducer: PropTypes.object.isRequired,
+    configReducer: PropTypes.object.isRequired,
+    fetchData: PropTypes.func.isRequired,
+    clearData: PropTypes.func.isRequired,
+  };
+
+  function mapStateToProps(state) {
+    return {
+      schemaReducer: state.schemaReducer,
+      detailReducer: state.detailReducer,
+      configReducer: state.configReducer
+    };
+  }
+
+
+  return connect(mapStateToProps, {
+    initialize,
+    fetchData,
+    clearData,
+    deleteData,
+    updateData
+  })(DetailView);
 };
 
-function mapStateToProps(state) {
-  return {
-    schemaReducer: state.schemaReducer,
-    detailReducer: state.detailReducer,
-    configReducer: state.configReducer
-  };
-}
-
-
-export default connect(mapStateToProps, {
-  initialize,
-  fetchData,
-  clearData,
-  deleteData,
-  updateData
-})(DetailView);
+export default getDetailView();
