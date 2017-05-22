523662e64e7687bae65006325642a09966b677db
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-05-22 15:26:40 +0200

    Added fetch UI:schema and apply it to detail dialog.
    ESI-13060

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import '../css/sass/main.scss';
 import Root from './app/Root';
 import createStore from './app/store';
 import {fetchConfig} from './config/ConfigActions';
+import {fetchUiSchema} from './uiSchema/UiSchemaActions';
 import {updateLocation} from './location/LocationActions';
 
 
@@ -17,11 +18,12 @@ store.unsubscribeHistory = hashHistory.listen(updateLocation(store));
 console.log(`Gohan version: ${VERSION.gohanWebUI.version}, repo tag: ${VERSION.gohanWebUI.tag}`);
 
 store.dispatch(fetchConfig()).then(() => {
-
-  render(
-    (
-      <Root store={store} history={hashHistory} />
-    ),
-    document.getElementById('root')
-  );
+  store.dispatch(fetchUiSchema()).then(() => {
+    render(
+      (
+        <Root store={store} history={hashHistory}/>
+      ),
+      document.getElementById('root')
+    );
+  });
 });
