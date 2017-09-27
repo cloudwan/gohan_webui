6d3c3f91197750f21b93aa2d8fe97b7a61770dc6
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-09-27 09:47:08 +0200

    Improved build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
-const process = require('process');
 const path = require('path');
 const gitSync = require('git-rev-sync');
+const process = require('process');
 const webpack = require('webpack');
+const HappyPack = require('happypack');
+const NyanProgressPlugin = require('nyan-progress-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const pkg = require('./package.json');
-
+const happyThreadPool = HappyPack.ThreadPool({size: 4});
 const devServerPort = 8080;
 const devServerHostname = 'localhost';
 const sourcePath = path.join(__dirname, '/src');
 const outputPath = path.join(__dirname, '/dist');
 const ENV = process.env.NODE_ENV;
+const isDevelopment = !ENV || ENV.toLowerCase() !== 'production';
 
-const version = () => ({
-  hash: gitSync.long(),
-  tag: gitSync.tag(),
-  version: pkg.version
-});
+process.traceDeprecation = true; // For better debuging purpose.
 
-module.exports = {
+function version() {
+  return {
+    hash: gitSync.long(),
+    tag: gitSync.tag(),
+    version: pkg.version
+  };
+}
+
+const config = {
   context: __dirname,
-  entry: [
-    'babel-polyfill',
-    './src/index'
-  ],
+  entry: {
+    polyfill: 'babel-polyfill',
+    main: './src/index'
+  },
   output: {
     path: outputPath,
     filename: '[name].[hash].js',
@@ -38,39 +46,18 @@ module.exports = {
         include: [
           __dirname
         ],
-        use: 'babel-loader'
+        use: 'happypack/loader?id=jsx'
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              query: {
-                sourceMap: true,
-                modules: true
-              }
-            }
-          ]
+          use: 'happypack/loader?id=css'
         })
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
+          use: 'happypack/loader?id=scss'
         })
       },
       {
@@ -98,24 +85,71 @@ module.exports = {
     ]
   },
   plugins: [
+    new NyanProgressPlugin(),
+    new HappyPack({
+      id: 'jsx',
+      threadPool: happyThreadPool,
+      verbose: false,
+      loaders: [
+        {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      ]
+    }),
+    new HappyPack({
+      id: 'css',
+      threadPool: happyThreadPool,
+      verbose: false,
+      loaders: [
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: isDevelopment,
+            minimize: !isDevelopment,
+            localIdentName: isDevelopment ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]',
+            modules: true
+          }
+        }
+      ]
+    }),
+    new HappyPack({
+      id: 'scss',
+      threadPool: happyThreadPool,
+      verbose: false,
+      loaders: [
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: isDevelopment,
+            minimize: !isDevelopment
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDevelopment,
+            minimize: !isDevelopment
+          }
+        }
+      ]
+    }),
     new ExtractTextPlugin({
       filename: '[name].[contenthash].css',
-      allChunks: true
+      allChunks: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
     }),
     new CopyWebpackPlugin([
-      {
-        from: 'src/config.json',
-        to: 'config.json'
-      },
       {
         from: 'locales',
         to: 'locales'
       }
     ]),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body'
-    }),
     new webpack.DefinePlugin({
       process: {
         env: {
@@ -129,15 +163,12 @@ module.exports = {
       name: 'vendor',
       minChunks: module => module.context && module.context.indexOf('node_modules') !== -1
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest'
-    }),
     new webpack.optimize.OccurrenceOrderPlugin()
   ],
   performance: {
     hints: false
   },
-  devtool: 'source-map',
+  devtool: 'inline-source-map',
   devServer: {
     host: devServerHostname,
     port: devServerPort,
@@ -152,6 +183,57 @@ module.exports = {
       timings: true,
       version: true,
       warnings: true
-    },
+    }
   }
 };
+
+if (!isDevelopment) {
+  config.plugins.push(
+    new UglifyJSPlugin({
+      sourceMap: isDevelopment,
+      parallel: true,
+      uglifyOptions: {
+        mangle: {
+          toplevel: !isDevelopment
+        },
+        output: {
+          beautify: isDevelopment,
+          ecma: 6
+        },
+        minimize: !isDevelopment,
+        compress: {
+          sequences: true,
+          properties: true,
+          dead_code: true, // eslint-disable-line camelcase
+          drop_debugger: true, // eslint-disable-line camelcase
+          unsafe: true,
+          conditionals: true,
+          comparisons: true,
+          evaluate: true,
+          booleans: true,
+          loops: true,
+          unused: true,
+          hoist_funs: true, // eslint-disable-line camelcase
+          hoist_vars: false, // eslint-disable-line camelcase
+          if_return: true, // eslint-disable-line camelcase
+          join_vars: true, // eslint-disable-line camelcase
+          cascade: true,
+          side_effects: true, // eslint-disable-line camelcase
+          warnings: true,
+          global_defs: {} // eslint-disable-line camelcase
+        }
+      }
+    })
+  );
+} else {
+  config.plugins.push(
+    new CopyWebpackPlugin([
+      {
+        from: 'src/config.json',
+        to: 'config.json'
+      }
+    ])
+  );
+}
+
+module.exports = config;
