545551f889b1f88624c35e47362deaca76cdbe86
Michał Damięcki <michal.damiecki@codilime.com>
2017-10-04 12:33:47 +0200

    Added breadcrumb to navbar. ESI-14577

---
+++ src/breadcrumb/breadcrumbReducer.js
@@ -0,0 +1,29 @@
+import {
+  UPDATE_FULFILLED,
+  UPDATE_FAILURE,
+} from './breadcrumbActionTypes';
+
+export const breadcrumbReducer = (state = {data: []}, action) => {
+  const {
+    type,
+    data,
+    error,
+  } = action;
+
+  switch (type) {
+    case UPDATE_FULFILLED:
+      return {
+        ...state,
+        data,
+      };
+    case UPDATE_FAILURE:
+      return {
+        ...state,
+        error
+      };
+    default:
+      return state;
+  }
+};
+
+export default breadcrumbReducer;
