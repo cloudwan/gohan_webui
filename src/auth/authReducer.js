75ba9a8e4d4ca1bd170e1288c289ccf3a52dacee
Katarzyna Fokow <sealionkat@gmail.com>
2017-11-02 10:25:04 +0100

    Added showing token renewal component.

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -4,29 +4,35 @@ import {
   LOGIN_SUCCESS,
   LOGIN_ERROR,
   FETCH_TENANTS_SUCCESS,
-  CLEAR_STORAGE
+  CLEAR_STORAGE,
+  SHOW_TOKEN_RENEWAL,
+  HIDE_TOKEN_RENEWAL
 } from './AuthActionTypes';
 
 export default function authReducer(state = {
   inProgress: true,
-  logged: false
+  logged: false,
+  showTokenRenewal: false
 }, action) {
   switch (action.type) {
     case CLEAR_STORAGE:
       return {
         inProgress: false,
-        logged: false
+        logged: false,
+        showTokenRenewal: false
       };
     case LOGOUT:
       return {
         inProgress: false,
-        logged: false
+        logged: false,
+        showTokenRenewal: false
       };
     case LOGIN_INPROGRESS:
       return {
         ...state,
         inProgress: true,
-        logged: false
+        logged: false,
+        showTokenRenewal: false
       };
     case LOGIN_SUCCESS:
       return {
@@ -35,21 +41,33 @@ export default function authReducer(state = {
         tokenExpires: action.data.tokenExpires,
         tenant: action.data.tenant,
         user: action.data.user,
-        inProgress: false
+        inProgress: false,
+        showTokenRenewal: false
       };
     case LOGIN_ERROR:
       return {
         ...state,
         inProgress: false,
-        logged: false
+        logged: false,
+        showTokenRenewal: false
       };
     case FETCH_TENANTS_SUCCESS:
       return {
         ...state,
         tenants: [...action.data],
         inProgress: false,
-        logged: true
-
+        logged: true,
+        showTokenRenewal: false
+      };
+    case SHOW_TOKEN_RENEWAL:
+      return {
+        ...state,
+        showTokenRenewal: true
+      };
+    case HIDE_TOKEN_RENEWAL:
+      return {
+        ...state,
+        showTokenRenewal: false
       };
     default:
       return state;
