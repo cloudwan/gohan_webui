a7ff81643dbb8ff2973b2c52ea17e81d5fadb311
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-11-27 10:46:22 +0100

    Fixed class names in DetailView component and props propagation.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -55,70 +55,72 @@ export default class Detail extends Component {
     } = this.props;
 
     return (
-      <div className="pt-card pt-elevation-0 detail">
-        <div className="detail-header">
-          <h2 className="title">{title}</h2>
-          <div className='actions pt-button-group pt-minimal'>
-            <Button className={'pt-minimal'}
-              iconName="edit"
-              text={'Edit'}
-              onClick={this.handleEditClick}
-            />
-            <Button className={'pt-minimal'}
-              iconName="trash"
-              text={'Delete'}
-              onClick={this.handleRemoveClick}
-            />
-            {!isEmpty(actions) && (
-              <CustomActions actions={actions}
-                baseUrl={url}
-                id={id}
+      <div className="detail-container">
+        <div className="pt-card pt-elevation-0 detail">
+          <div className="detail-header">
+            <h2 className="title">{title}</h2>
+            <div className='actions pt-button-group pt-minimal'>
+              <Button className={'pt-minimal'}
+                iconName="edit"
+                text={'Edit'}
+                onClick={this.handleEditClick}
               />
-            )}
+              <Button className={'pt-minimal'}
+                iconName="trash"
+                text={'Delete'}
+                onClick={this.handleRemoveClick}
+              />
+              {!isEmpty(actions) && (
+                <CustomActions actions={actions}
+                  baseUrl={url}
+                  id={id}
+                />
+              )}
+            </div>
           </div>
-        </div>
-        <div className="detail-content">
-          {schema.propertiesOrder.map((key, index) => {
-            const property = schema.properties[key];
-            const propertyValue = data[key];
+          <div className="detail-content">
+            {schema.propertiesOrder.map((key, index) => {
+              const property = schema.properties[key];
+              const propertyValue = data[key];
 
-            if (property.view && !property.view.includes('detail')) {
-              return null;
-            }
+              if (property.view && !property.view.includes('detail')) {
+                return null;
+              }
 
-            if (property.type === 'array' || property.type === 'object') {
+              if (property.type === 'array' || property.type === 'object') {
+                return (
+                  <div className="gohan-detail-property" key={index}>
+                    <div className="property-title">{property.title}: </div>
+                    <CodeMirror className="cm-s-monokai" value={jsyaml.safeDump(propertyValue)}
+                      options={{
+                        mode: 'yaml',
+                        lineNumbers: true,
+                        readOnly: true,
+                        cursorBlinkRate: -1
+                      }}
+                    />
+                  </div>
+                );
+              }
+
               return (
                 <div className="gohan-detail-property" key={index}>
                   <div className="property-title">{property.title}: </div>
-                  <CodeMirror className="cm-s-monokai" value={jsyaml.safeDump(propertyValue)}
-                    options={{
-                      mode: 'yaml',
-                      lineNumbers: true,
-                      readOnly: true,
-                      cursorBlinkRate: -1
-                    }}
-                  />
+                  <div className="property-value">{this.renderPropertyValueAsText(propertyValue)}</div>
                 </div>
               );
-            }
-
-            return (
-              <div className="gohan-detail-property" key={index}>
-                <div className="property-title">{property.title}: </div>
-                <div className="property-value">{this.renderPropertyValueAsText(propertyValue)}</div>
+            })}
+            <Button className={'pt-minimal toggle-request-form'}
+              iconName={isApiRequestFormOpen ? 'chevron-up' : 'chevron-down'}
+              text={'API Request Form'}
+              onClick={this.handleApiRequestFormToggle}
+            />
+            {
+              isApiRequestFormOpen && <div className="detail-request-form">
+                <ApiRequest baseUrl={`${gohanUrl}${this.props.schema.url}/${data.id}`} />
               </div>
-            );
-          })}
-          <Button className={'pt-minimal toggle-request-form'}
-            iconName={isApiRequestFormOpen ? 'chevron-up' : 'chevron-down'}
-            text={'API Request Form'}
-            onClick={this.handleApiRequestFormToggle}
-          />
-          {
-            isApiRequestFormOpen && <div className="detail-request-form">
-              <ApiRequest baseUrl={`${gohanUrl}${this.props.schema.url}/${data.id}`} />
-            </div>
-          }
+            }
+          </div>
         </div>
       </div>
     );
