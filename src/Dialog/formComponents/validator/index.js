4716dcd9cec08dea425ae07832c9e26b3b578909
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-12-20 07:38:51 +0100

    Changed version of json-schema in validator from v6 to v4.
    ESI-16404

--- src/Dialog/formComponents/validator/index.js
+++ src/Dialog/formComponents/validator/index.js
@@ -1,6 +1,7 @@
 import {Validator} from 'jsonschema';
 import {FORMAT_REGEXPS} from 'jsonschema/lib/helpers';
 import Ajv from 'ajv';
+import metaSchema from 'ajv/lib/refs/json-schema-draft-04.json';
 
 import cidrFormat from './cidrFormat';
 import macFormat from './macFormat';
@@ -20,11 +21,22 @@ Validator.prototype.customFormats.cidr = cidrFormat;
 Validator.prototype.customFormats.uuid = uuidFormat;
 
 const ajv = new Ajv({
+  meta: false,
+  extendRefs: true,
+  unknownFormats: 'ignore',
   allErrors: true,
   formats: {
     cidr: cidrFormat,
     mac: macFormat
   }
 });
+
+// https://github.com/epoberezkin/ajv/issues/471
+ajv.addMetaSchema(metaSchema);
+ajv._opts.defaultMeta = metaSchema.id;
+ajv._refs['http://json-schema.org/schema'] = 'http://json-schema.org/draft-04/schema';
+ajv.removeKeyword('propertyNames');
+ajv.removeKeyword('contains');
+ajv.removeKeyword('const');
 
 export default ajv;
