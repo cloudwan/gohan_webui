9e62ea7885171502f492bb4ef438e74fb8429201
Kamil Stepczuk <k.stepczuk@hotmail.com>
2018-01-11 10:55:13 +0100

    Fixed 'select all' behavior after deleting.
    Added deletion animation.
    
    ESI-16561

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TableRow = ({children}) => {
+const TableRow = ({children, active}) => {
   return (
-    <tr>{children}</tr>
+    <tr {...{className: active ? 'active' : undefined}}>{children}</tr>
   );
 };
 
 TableRow.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  active: PropTypes.bool
 };
 
 export default TableRow;
