39165369dc77e60aa70ddfc47296e43d4da87272
Michał Damięcki <michal.damiecki@codilime.com>
2018-01-15 17:48:49 +0100

    Fixed breadcrumb and title for sampleView and NotFoundView. ESI-16686

--- src/breadcrumb/breadcrumbSelectors.js
+++ src/breadcrumb/breadcrumbSelectors.js
@@ -7,20 +7,8 @@ const breadcrumb = state => {
 
   return [];
 };
-const initialDocTitle = state => {
-  if (state.breadcrumbReducer && state.breadcrumbReducer.initialDocTitle) {
-    return state.breadcrumbReducer.initialDocTitle;
-  }
-
-  return '';
-};
 
 export const getBreadcrumbContent = createSelector(
   [breadcrumb],
   breadcrumb => breadcrumb
-);
-
-export const getInitialDocTitle = createSelector(
-  [initialDocTitle],
-  initialDocTitle => initialDocTitle
 );
