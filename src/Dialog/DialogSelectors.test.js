f431f130a10445f2dc4a0d596789ac2ef8e126a6
Katarzyna <sealionkat@users.noreply.github.com>
2018-01-25 12:14:09 +0100

    Added opened dialog selectors. ESI-16765

--- src/Dialog/DialogSelectors.test.js
+++ src/Dialog/DialogSelectors.test.js
@@ -70,4 +70,86 @@ describe('DialogSelectors', () => {
       ).should.equal('Test error message.');
     });
   });
+
+  describe('isAnyDialogOpen()', () => {
+    it('should return false', () => {
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: false,
+            d2: false
+          }
+        }
+      }, []).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: false,
+            d2: false
+          }
+        }
+      }).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: false,
+            d2: false
+          }
+        }
+      }, ['d3']).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: false,
+            d2: false
+          }
+        }
+      }, ['d1', 'd2']).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: true,
+            d2: false
+          }
+        }
+      }, ['d2', 'd3']).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {}
+        }
+      }, ['d2', 'd3']).should.equal(false);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {}
+        }
+      }).should.equal(false);
+    });
+
+    it('should return true', () => {
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: true,
+            d2: false
+          }
+        }
+      }, ['d1']).should.equal(true);
+
+      selectors.isAnyDialogOpen({
+        dialogReducer: {
+          dialogs: {
+            d1: true,
+            d2: false
+          }
+        }
+      }, ['d1', 'd2']).should.equal(true);
+    });
+  });
+
 });
