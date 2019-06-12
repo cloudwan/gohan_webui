88e0df062d8aa2fdd35f95f91ebf91277087712d
Michał Damięcki <michal.damiecki@codilime.com>
2019-06-12 14:51:36 +0200

    Fixed detail fetch when property has relation without schema. UIE-524

--- src/DetailView/DetailActionTypes.js
+++ src/DetailView/DetailActionTypes.js
@@ -4,6 +4,7 @@ export const FETCH = `${prefix}_FETCH`;
 export const FETCH_PARENTS = `${prefix}_FETCH_PARENTS`;
 export const FETCH_SUCCESS = `${prefix}_FETCH_SUCCESS`;
 export const FETCH_FAILURE = `${prefix}_FETCH_FAILURE`;
+export const FETCH_RELATION_FAILURE = `${prefix}_FETCH_RELATION_FAILURE`;
 export const FETCH_CANCELLED = `${prefix}_FETCH_CANCELLED`;
 export const CLEAR_DATA = `${prefix}_CLEAR_DATA`;
 export const DELETE = `${prefix}_DELETE`;
